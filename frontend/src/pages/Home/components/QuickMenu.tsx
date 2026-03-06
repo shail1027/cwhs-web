bc9af193b468d6b76ac0f9904426496adc0a7306
Yebin Lee <139226103+shail1027@users.noreply.github.com>
2026-03-06 17:14:25 +0900

    feat: quickmenu react icon

--- frontend/src/pages/Home/components/QuickMenu.tsx
+++ frontend/src/pages/Home/components/QuickMenu.tsx
@@ -4,40 +4,64 @@ import { Link } from "react-router-dom";
 
 import { FaSchool } from "react-icons/fa6";
 import { IoDocumentTextSharp } from "react-icons/io5";
-
+import { FaMapMarkedAlt } from "react-icons/fa";
+import { FaCalendarAlt } from "react-icons/fa";
 
 interface QuickMenuList {
-    id: number;
-    name: string;
-    icon: string;
+  id: number;
+  name: string;
+  icon: React.ElementType;
+  linkto: string;
 }
 
 const QML: QuickMenuList[] = [
-    {
-        id: 1,
-        name: "학교소개",
-        icon: "FaSchool"
-    },
-    {
-        id: 2,
-        name: "각종양식",
-        icon: "IoDocumentTextSharp"
-    },
-    {
-        id: 3,
-        name: "각종양식",
-        icon: "IoDocumentTextSharp"
-    }
-]
+  {
+    id: 1,
+    name: "학교소개",
+    icon: FaSchool,
+    linkto: "introduce",
+  },
+  {
+    id: 2,
+    name: "각종양식",
+    icon: IoDocumentTextSharp,
+    linkto: "#",
+  },
+  {
+    id: 3,
+    name: "학사일정",
+    icon: FaCalendarAlt,
+    linkto: "#",
+  },
+  {
+    id: 4,
+    name: "오시는 길",
+    icon: FaMapMarkedAlt,
+    linkto: "#",
+  },
+];
 
 const QuickMenu = () => {
-    return (
-        <div className={styles["container"]}>
-            <ul className={styles["list"]}>
-                {}
-            </ul>
-        </div>
-    );
+  return (
+    <div className={styles["container"]}>
+      <ul className={styles["list"]}>
+        {QML.map((props) => {
+          const Icon = props.icon;
+          return (
+            <li key={props.id}>
+              {" "}
+              <Link to={props.linkto}>
+                <div className={styles["list-item"]}>
+                  <Icon />
+                  <p>{props.name}</p>
+                </div>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default QuickMenu;
