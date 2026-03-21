867e364800d6364a32c11021f615fbd819e97ba2
Yebin Lee <139226103+shail1027@users.noreply.github.com>
2026-03-21 19:17:03 +0900

    feat: swagger

---
+++ backend/src/swagger.ts
@@ -0,0 +1,22 @@
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "천월고등학교 API 명세서",
+      version: "1.0.0",
+      description: "천월고 백엔드 API (Node.js + Express + Prisma)",
+    },
+    servers: [
+      {
+        url: "http://localhost:4000", 
+      },
+    ],
+  },
+  apis: ["./src/controllers/*.ts", "./src/routes/*.ts"], 
+};
+
+export const specs = swaggerJsdoc(options);
+export { swaggerUi };
