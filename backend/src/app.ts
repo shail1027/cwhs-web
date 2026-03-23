ca83a86a319acc2843cbe87574bf1aa4a0498357
Yebin Lee <139226103+shail1027@users.noreply.github.com>
2026-03-23 15:37:13 +0900

    feat: singup api

---
+++ backend/src/app.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import { signup } from "./controllers/authController";
+import { swaggerUi, specs } from "./swagger"; 
+
+const app = express();
+const PORT = 4000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+app.post("/api/auth/signup", signup);
+
+app.listen(PORT, () => {
+  console.log(`서버 실행 중: http://localhost:${PORT}`);
+  console.log(`API 명세서: http://localhost:${PORT}/api-docs`); 
+});
